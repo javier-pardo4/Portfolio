678fa3b6afab0d05a329bd161e875fce1baae3d1
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-10-09 22:28:32 +0200

    First structure of the portfolio

--- PortfolioJavi/src/app/app.component.ts
+++ PortfolioJavi/src/app/app.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Necesitas importar CommonModule en componentes standalone
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,  // Marca el componente como standalone
+  imports: [CommonModule]  // Importa los módulos que necesites
 })
-export class AppComponent {
-  title = 'PortfolioJavi';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    particlesJS('particles-js', {
+      "particles": {
+        "number": {
+          "value": 100,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": true
+        },
+        "size": {
+          "value": 5,
+          "random": true
+        },
+        "line_linked": {
+          "enable": false
+        },
+        "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "attract": {
+            "enable": false
+          }
+        }
+      }
+    });
+  }
 }
