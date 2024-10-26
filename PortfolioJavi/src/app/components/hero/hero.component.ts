898e803db6cd4b60dbbcf84b5da5e65f5d72de86
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-10-26 16:36:28 +0200

    Inicio

--- PortfolioJavi/src/app/components/hero/hero.component.ts
+++ PortfolioJavi/src/app/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,7 +7,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class  HeroComponent{
+export class HeroComponent implements OnInit, OnDestroy {
+  words: string[] = ['INGENIERO', 'INFORMATICO'];
+  displayText: string = '';
+  currentWordIndex: number = 0;
+  isDeleting: boolean = false;
+  typingTimeout: any;
 
+  ngOnInit() {
+    this.startTypingEffect();
+  }
 
+  ngOnDestroy() {
+    clearTimeout(this.typingTimeout);
+  }
+
+  startTypingEffect() {
+    const currentWord = this.words[this.currentWordIndex];
+
+    if (this.isDeleting) {
+      this.displayText = currentWord.substring(0, this.displayText.length - 1);
+    } else {
+      this.displayText = currentWord.substring(0, this.displayText.length + 1);
+    }
+
+    const typingSpeed = this.isDeleting ? 100 : 200;
+
+    if (!this.isDeleting && this.displayText === currentWord) {
+      this.typingTimeout = setTimeout(() => {
+        this.isDeleting = true;
+        this.startTypingEffect();
+      }, 1000);
+    } else if (this.isDeleting && this.displayText === '') {
+      this.isDeleting = false;
+      this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+      this.typingTimeout = setTimeout(() => this.startTypingEffect(), 500);
+    } else {
+      this.typingTimeout = setTimeout(() => this.startTypingEffect(), typingSpeed);
+    }
+  }
 }
