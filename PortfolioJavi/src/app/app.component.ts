61be01d5457367f6ae1931782343aa63d7df7383
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-10-26 14:38:37 +0200

    Inicio

--- PortfolioJavi/src/app/app.component.ts
+++ PortfolioJavi/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Necesitas importar CommonModule en componentes standalone
+import { HeaderComponent } from './components/header/header.component'; // Importa tu componente
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 declare var particlesJS: any;
 
@@ -8,12 +15,12 @@ declare var particlesJS: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,  // Marca el componente como standalone
-  imports: [CommonModule]  // Importa los módulos que necesites
+  imports: [CommonModule, HeaderComponent, HeroComponent, AboutComponent, ContactComponent, ProjectsComponent, FooterComponent]  // Importa los módulos que necesites
 })
 export class AppComponent implements OnInit {
   title = 'PortfolioJavi';
   
     ngOnInit() {
-      particlesJS.load('particles-js', 'assets/particles.json', null);
+      
     }
 }
