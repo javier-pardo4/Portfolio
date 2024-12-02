8a38dd114eca150f540ef3dc02c6fbda6cf2bea8
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-12-02 14:00:25 +0100

    social link

--- PortfolioJavi/src/app/components/hero/hero.component.ts
+++ PortfolioJavi/src/app/components/hero/hero.component.ts
@@ -4,19 +4,26 @@ import { AboutComponent } from '../about/about.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { ProjectsComponent } from '../projects/projects.component';
 
-
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [CommonModule, AboutComponent, TechnologiesComponent, ProjectsComponent],
-  templateUrl: './hero.component.html', 
-  styleUrls: ['./hero.component.css'] // Asegúrate de tener la ruta correcta de tu archivo CSS
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
 })
-export class HeroComponent{
+export class HeroComponent {
   selectedTab: string = 'aboutMe'; // Establecemos 'Sobre mí' como pestaña inicial.
 
   // Método para cambiar la pestaña seleccionada
   selectTab(tab: string) {
     this.selectedTab = tab;
+  }
+
+  // Método para redirigir según el botón clickeado
+  redirectToLink(event: any) {
+    const link = event.target.getAttribute('data-link');
+    if (link) {
+      window.open(link, '_blank'); // Redirige al enlace en una nueva pestaña
+    }
   }
 }
