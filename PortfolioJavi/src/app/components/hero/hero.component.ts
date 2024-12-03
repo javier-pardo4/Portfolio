9bd3fe53d0ef88845684ee3f933a75ffd2159da3
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-12-03 23:28:11 +0100

    footer

--- PortfolioJavi/src/app/components/hero/hero.component.ts
+++ PortfolioJavi/src/app/components/hero/hero.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from '../about/about.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-hero',
@@ -13,6 +14,26 @@ import { ProjectsComponent } from '../projects/projects.component';
 })
 export class HeroComponent {
   selectedTab: string = 'aboutMe'; // Establecemos 'Sobre mí' como pestaña inicial.
+  carouselOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  };
+
   constructor(private renderer: Renderer2) {}
 
   // Método para cambiar la pestaña seleccionada
