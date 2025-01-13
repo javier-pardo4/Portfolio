efb3393baa0c23882b6f7f5aaa1f86460af8d02c
javier-pardo4 <javier.pardo4@alu.uclm.es>
2025-01-13 14:42:39 +0100

    ResponsiveBanner

--- PortfolioJavi/src/app/components/hero/hero.component.ts
+++ PortfolioJavi/src/app/components/hero/hero.component.ts
@@ -1,9 +1,8 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from '../about/about.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { ProjectsComponent } from '../projects/projects.component';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-hero',
@@ -14,25 +13,6 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class HeroComponent {
   selectedTab: string = 'aboutMe'; // Establecemos 'Sobre mí' como pestaña inicial.
-  carouselOptions: OwlOptions = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
-    }
-  };
 
   constructor(private renderer: Renderer2) {}
 
